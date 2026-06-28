548d8b9e98f5e453a48b3958966bbf9b4528166b
Tinoskk03Tech <kossivitinek@gmail.com>
2026-06-28 18:11:41 +0000

    Fin de la partie IIFE : Immediately Invoked Function Expression

--- Fonctions/expressionFonction.js
+++ Fonctions/expressionFonction.js
@@ -20,3 +20,18 @@ console.log("La moyenne est : " + resultatMoyenne); // Affiche la moyenne
 var resultats = moyenne([10, 20, 30, 40, 50]);
 
 console.log("La moyenne est : " + resultats); // Affiche la moyenne
+
+
+//IIFE : Immediately Invoked Function Expression
+
+(function IIFE(global, $){
+    var i = 0;
+    while(i > 0){
+        console.log(i);
+    }
+    global.somme = function calculerSomme(valeur1, valeur2){
+        return valeur1 + valeur2;
+    };
+    console.log("La somme est : " + somme(10, 5)); // Affiche la somme
+})(window, jQuery); // On passe window et jQuery en paramètres pour éviter les conflits avec d'autres bibliothèques
+
