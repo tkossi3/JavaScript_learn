c0f5b23d9972e6f583e5677f9959dab4f3ad4f07
Tinoskk03Tech <kossivitinek@gmail.com>
2026-06-28 18:04:15 +0000

    Différence entre les expressions de fonction et de déclaration de fonction

---
+++ Fonctions/expressionFonction.js
@@ -0,0 +1,22 @@
+
+function somme(valeur1, valeur2){
+    return valeur1 + valeur2;
+}
+
+var moyenne = function claculerMoyenne(mesNotes){
+    var somme = 0;
+    for (var i = 0; i < mesNotes.length; i++) {
+        somme += mesNotes[i];
+    }
+    return somme / mesNotes.length;
+}
+
+var notes = [15, 18, 12, 14, 16];
+
+var resultatMoyenne = moyenne(notes);
+
+console.log("La moyenne est : " + resultatMoyenne); // Affiche la moyenne
+
+var resultats = moyenne([10, 20, 30, 40, 50]);
+
+console.log("La moyenne est : " + resultats); // Affiche la moyenne
